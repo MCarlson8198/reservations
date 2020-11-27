ad12f67fb6e0eb5436d779e9818a73105fae39e5
Matt Carlson <matt.carlson@gaia.com>
2020-11-27 16:48:06 -0700

    setting up redux and middle ware

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { connect as connectRedux } from 'react-redux';
+import { getBoundActions } from './redux/actions/index';
 
-function App() {
+function App(props) {
+  const { switchUser, currentUserSide, gameWon } = props
+  const newAction = () => {
+    switchUser('X')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Current User: {currentUserSide}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+        GameIsWon: {gameWon}
+        </p>
+        <button onClick={() => newAction()}>CLICK</button>
+        <button onClick={() => switchUser('O')}>For O</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default connectRedux(
+  state => ({
+    gameData: state.gameData,
+    gameWon: state.gameData.get('gameIsWon'),
+    winningConditions: state.gameData.get('winningArr'),
+    tileData: state.gameData.get('tileData'),
+    currentUserSide: state.gameData.get('userSide'),
+    userXData: state.gameData.get('userXChoices'),
+    userOData: state.gameData.get('userOChoices'),
+  }),
+  (dispatch) => {
+    const actions = getBoundActions(dispatch)
+    return {
+      userO: actions.game.userO,
+      userX: actions.game.userX,
+      switchUser: actions.game.switchUser,
+      setTileData: actions.game.setTileData,
+      setGameWon: actions.game.setGameWon,
+    }
+  },
+)(App);
