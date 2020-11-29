f561c6e1ae1222ba6a113cc7b660d64af1257556
Matt Carlson <matt.carlson@gaia.com>
2020-11-29 09:43:30 -0700

    clean up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { describe, it } from 'mocha'
+import { assert } from 'chai'
+import { setPartySize } from './redux/actions'
+import { SET_PARTY_SIZE } from './redux/types'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Reservations/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('reservations actions', () => {
+  describe('Function: setPartySize()', () => {
+    it('should create an action', () => {
+      const size = 3
+      const action = {
+        type: SET_PARTY_SIZE,
+        payload: size,
+      }
+      assert.deepEqual(
+        action,
+        setPartySize(size),
+      )
+    })
+  })
+})
